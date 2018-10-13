e5cc2d4d50faad1c9bc76087899c870cfc66cbde
jugnu1896 <44043696+jugnu1896@users.noreply.github.com>
2018-10-13 21:58:34 +0530

    Update PlayersObject.php

--- PlayersObject.php
+++ PlayersObject.php
@@ -11,6 +11,9 @@
 
       Feel free to add as many additional classes and interfaces as you see fit.
 
+      Note: Please create a fork of the https://github.com/BrandonLegault/exercise repository and commit your changes
+        to your fork. The goal here is not 100% correctness, but instead a glimpse into how you
+        approach refactoring/redesigning bad code. Commit often to your fork.
 
 */
 
@@ -96,42 +99,16 @@ class PlayersObject implements IReadWritePlayers {
     function getPlayerDataArray() {
 
         $players = [];
-
-        $jonas = new \stdClass();
-        $jonas->name = 'Jonas Valenciunas';
-        $jonas->age = 26;
-        $jonas->job = 'Center';
-        $jonas->salary = '4.66m';
-        $players[] = $jonas;
-
-        $kyle = new \stdClass();
-        $kyle->name = 'Kyle Lowry';
-        $kyle->age = 32;
-        $kyle->job = 'Point Guard';
-        $kyle->salary = '28.7m';
-        $players[] = $kyle;
-
-        $demar = new \stdClass();
-        $demar->name = 'Demar DeRozan';
-        $demar->age = 28;
-        $demar->job = 'Shooting Guard';
-        $demar->salary = '26.54m';
-        $players[] = $demar;
-
-        $jakob = new \stdClass();
-        $jakob->name = 'Jakob Poeltl';
-        $jakob->age = 22;
-        $jakob->job = 'Center';
-        $jakob->salary = '2.704m';
-        $players[] = $jakob;
-
+        $data = file_get_contents ("./playerdata.json");
+        $json = json_decode($data, true);
+        foreach ($json as $key => $value) {
+                    $playerData->name = $value[$key]["name"];
+                    $playerData->age = $value[$key]["age"];
+                    $playerData->job = $value[$key]["job"];
+                    $playerData->salary = $value[$key]["salary"];
+                    $players[] = $playerData;
+             }
         return $players;
-
-    }
-
-    function getPlayerDataJson() {
-        $json = '[{"name":"Jonas Valenciunas","age":26,"job":"Center","salary":"4.66m"},{"name":"Kyle Lowry","age":32,"job":"Point Guard","salary":"28.7m"},{"name":"Demar DeRozan","age":28,"job":"Shooting Guard","salary":"26.54m"},{"name":"Jakob Poeltl","age":22,"job":"Center","salary":"2.704m"}]';
-        return $json;
     }
 
     function getPlayerDataFromFile($filename) {
